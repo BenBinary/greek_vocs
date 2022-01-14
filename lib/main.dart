6e030e7141500c0bd799b3c195b298382d11cc77
BenBinary <43401683+BenBinary@users.noreply.github.com>
2022-01-14 14:48:54 +0200

    compiles on Android

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Greek Vocs'),
         ), body: Center(
-        child: Text(wordPair.asPascalCase)
+        child: RandomWords(),
       ),
       )
 
@@ -51,12 +51,45 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+
+    return ListTile(title:
+    Text(pair.asPascalCase,
+        style: _biggerFont),
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
 
+      final wordPair = WordPair.random();
+      return Text(wordPair.asPascalCase);
 
-    return Container();
   }
+
+
 }
 
 
