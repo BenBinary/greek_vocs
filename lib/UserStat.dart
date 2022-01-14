28e740e1c398c8495040348bd6de674150a60aae
BenBinary <43401683+BenBinary@users.noreply.github.com>
2022-01-14 16:54:42 +0200

    fill the personal section

--- lib/UserStat.dart
+++ lib/UserStat.dart
@@ -9,12 +9,51 @@ class UserStat extends StatefulWidget {
 
 class _UserStatState extends State<UserStat> {
 
+  Widget _userProperties() {
+
+    return ListView.builder(itemBuilder: (context, i) {
+      
+      return MaterialBanner(content: Text('Tach'), actions: <Widget>[TextButton(onPressed: null, child: Text('Open'))]);
+
+    });
+  }
+
+  Widget _userStatistics() {
+
+    final lv = ListView(
+      padding: const EdgeInsets.all(5),
+      children: [Container(
+        height: 50,
+        color: Colors.amber[500],
+        child: const Center(child: Text('Stat 1')),
+      ), Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Stat 1')),
+        ), Container(
+        height: 50,
+        color: Colors.amber[700],
+        child: const Center(child: Text('Stat 1')),
+      ),
+      ],
+    );
+
+    return lv;
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
       title: const Text('User Profile'),
-    ));
+
+    ),
+    body:// _userProperties(),
+        _userStatistics()
+    //const Text('Hello'),
+   // const MaterialBanner(content: Text('Hello'), actions: <Widget>[TextButton(onPressed: null, child: Text('Open'))])
+    );
   }
 }
 
